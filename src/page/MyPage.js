97e0ffaaa127ad2129a4390aeabfdee4769047ba
hunjoonrhee <hunjoonrhee90@gmail.com>
2024-06-15 16:17:01 +0200

    my page with no orders

--- src/page/MyPage.js
+++ src/page/MyPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Card, Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { orderActions } from '../action/orderAction';
 import OrderStatusCard from '../component/OrderStatusCard';
@@ -29,7 +29,15 @@ const MyPage = ({ user }) => {
   return (
     <Container className="status-card-container">
       {myOrders.length === 0 ? (
-        <> no orders </>
+        <Card className="text-center">
+          <Card.Body>
+            <Card.Title>No Orders Found</Card.Title>
+            <Card.Text>You haven't placed any orders yet. Start shopping now!</Card.Text>
+            <Button variant="primary" href="/">
+              Go to Shop
+            </Button>
+          </Card.Body>
+        </Card>
       ) : (
         myOrders.map((order) => {
           return <OrderStatusCard order={order} />;
